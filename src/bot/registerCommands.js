57d75ed9a7ccae3b60670c78714f83b2f9da39f3
JadenF5 <jadenfern05@gmail.com>
2026-06-12 16:22:06 -0400

    commands start

---
+++ src/bot/registerCommands.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const logger = require('../utils/logger');
+
+const commands = [
+  {
+    name: 'ask',
+    description: '',
+    options: [
+      {
+        name: 'question',
+        type: 3,
+        description: '',
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'help',
+    description: 'Show all available commands',
+    options: []
+  }
+];
+
+async function registerCommands() {
+  const token = process.env.DISCORD_TOKEN;
+  const appId = process.env.DISCORD_APP_ID;
+
+  if (!token) {
+    logger.error('DISCORD_TOKEN is not set');
+    process.exit(1);
+  }
+  if (!appId) {
+    logger.error('DISCORD_APP_ID is not set');
+    process.exit(1);
+  }
+
+  console.log('Token exists:', !!token);
+  console.log('Token length:', token?.length);
+  console.log('App ID:', appId);
+
+  const rest = new REST({ version: '10' }).setToken(token);
+
+  try {
+    logger.info('Registering slash commands...');
+    const data = await rest.put(Routes.applicationCommands(appId), {
+      body: commands
+    });
+    logger.info('Successfully registered', data.length, 'command(s)');
+    process.exit(0);
+  } catch (err) {
+    logger.error('Failed to register commands:', err.message);
+    process.exit(1);
+  }
+}
+
+registerCommands();
